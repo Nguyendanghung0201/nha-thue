41e318c3fc66b40d02adc46593e67102ea807709
danghung <danghung0201@gmail.com>
2023-03-23 16:14:00 +0700

    update

--- app/modules/location/model.js
+++ app/modules/location/model.js
@@ -1,16 +1,40 @@
 const repLocation = require('./repository')
 exports.list_province = async () => {
-    return await repLocation.getListProvice()
+    let result = await repLocation.getListProvice()
+    return {
+        status: true,
+        msg: "success",
+        code: 0,
+        data: result
+    };
 }
 
 exports.list_city = async(query)=>{
-    return await repLocation.getListCity(query.param.id)
+    let result = await repLocation.getListCity(query.param.id)
+    return {
+        status: true,
+        msg: "success",
+        code: 0,
+        data: result
+    };
 }
 
 exports.list_town = async(query)=>{
-    return await repLocation.getListTown(query.param.id)
+    let result = await repLocation.getListTown(query.param.id)
+    return {
+        status: true,
+        msg: "success",
+        code: 0,
+        data: result
+    };
 }
 exports.list_along = async(query)=>{
-    return await repLocation.getListAlong(query.param.id)
+    let result = await repLocation.getListAlong(query.param.id)
+    return {
+        status: true,
+        msg: "success",
+        code: 0,
+        data: result
+    };
 }
 
