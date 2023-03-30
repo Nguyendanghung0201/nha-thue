a0df5988f8052c1e5ceaef7658de2e54ba79d933
danghung <danghung0201@gmail.com>
2023-03-30 20:27:33 +0700

    add

--- app/modules/my_build/repository.js
+++ app/modules/my_build/repository.js
@@ -18,8 +18,8 @@ class my_build_repository {
     async getList(user_id, page) {
         return await db('my_build')
             .leftJoin('building2', 'my_build.buiding_id', 'building2.id')
-            .select('building2.*', 'my_build.id').where('my_build.user_id', user_id)
-            .orderBy('my_build.id', 'desc').paginate({ perPage: 20, isLengthAware: true, currentPage: page })
+            .select('building2.*', 'my_build.id as build_id').where('my_build.user_id', user_id)
+            .orderBy('build_id', 'desc').paginate({ perPage: 20, isLengthAware: true, currentPage: page })
     }
     async delete(user_id, id) {
         return await db('my_build').where('user_id', user_id).andWhere('id', id).delete()
