4cc1a47717b8e98962862d07b7119db02fdbbf95
danghung <danghung0201@gmail.com>
2023-06-30 08:49:23 +0700

    add

--- app/admin/khuyenmai/model.js
+++ app/admin/khuyenmai/model.js
@@ -1,19 +1,29 @@
 const rep = require('./repository')
-exports.add = async function(query){
+exports.add = async function (query) {
     let result = await rep.add(query)
     return {
-        status:true,
-        code:0,
-        msg:'success',
-        data:result
+        status: true,
+        code: 0,
+        msg: 'success',
+        data: result
     }
 }
-exports.delete = async function(query){
+exports.delete = async function (query) {
     let result = await rep.delete(query.id)
     return {
-        status:true,
-        code:0,
-        msg:'success',
-        data:result
+        status: true,
+        code: 0,
+        msg: 'success',
+        data: result
+    }
+}
+
+exports.list = async function () {
+    let result = await rep.list()
+    return {
+        status: true,
+        code: 0,
+        msg: 'success',
+        data: result
     }
 }
