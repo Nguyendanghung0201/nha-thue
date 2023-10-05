6402c84939c67c486d68644a3b8d8476dccfae6f
danghung <danghung0201@gmail.com>
2023-10-05 10:50:00 +0700

    ADD

--- public/chunk-2d0a3587.1693876237413.92b1493c.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0a3587"],{"027f":function(n,u,e){"use strict";e.r(u);var t=function(){var n=this,u=n._self._c;return u("div",[u("router-view")],1)},r=[],c={},i=c,l=e("2877"),o=Object(l["a"])(i,t,r,!1,null,null,null);u["default"]=o.exports}}]);
